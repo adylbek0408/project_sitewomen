05e47462e93fb98fc071656c1235a534732ff2a6
adylbek0408 <adylbek0408@icloud.com>
2024-01-15 00:42:28 +0300

    lesson #10

---
+++ sitewomen/women/views.py
@@ -0,0 +1,52 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.template.loader import render_to_string
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+menu = ["О сайте", "Добавьте статью", "Обратная связь", "Войти"]
+
+
+def index(request):
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'asd', True],
+            'set': {1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+            }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2023:
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+
