7d6c912c2b7e11ff7df868288bb4437a319fb8ba
adylbek0408 <adylbek0408@icloud.com>
2024-01-17 00:37:05 +0300

    lesson # 13

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,14 +5,18 @@ from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 
 
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелина Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': True},
-    {'id': 3, 'title': 'Джуля Робертс', 'content': 'Биография Джуля Робертс', 'is_published': True},
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статьи', 'url_name': 'addpage'},
+    {'title': 'Обротная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
 ]
 
-
-menu = ["О сайте", "Добавьте статью", "Обратная связь", "Войти"]
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелина Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джуля Робертс', 'content': 'Биография Джуля Робертс', 'is_published': True},
+]
 
 
 def index(request):
@@ -29,19 +33,20 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        return HttpResponsePermanentRedirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 
 def page_not_found(request, exception):
