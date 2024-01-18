df80bb2fe6de9525f23d3b2f166012d62d1d6476
adylbek0408 <adylbek0408@icloud.com>
2024-01-18 13:32:15 +0300

    lesson #22

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 
+from .models import Women
 
 menu = [
     {'title': 'О сайте', 'url_name': 'about'},
@@ -28,11 +29,10 @@ cats_db = [
 
 
 def index(request):
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
+    posts = Women.objects.filter(is_published=1)
     data = {'title': 'Главная страница',
             'menu': menu,
-            'posts': data_db,
+            'posts': posts,
             'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -42,8 +42,16 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
@@ -61,6 +69,7 @@ def show_category(request, cat_id):
             'cat_selected': cat_id,
     }
     return render(request, 'women/index.html', context=data)
+
 
 def login(request):
     return HttpResponse("Авторизация")
