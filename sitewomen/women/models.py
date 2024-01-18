be63be3c7aab4c4bf108e3dff84bfd526fe86f30
adylbek0408 <adylbek0408@icloud.com>
2024-01-18 12:49:24 +0300

    lesson #21

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -6,8 +6,14 @@ class Women(models.Model):
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_publish = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
 
